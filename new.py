228b980adbc6afbd825258ceeebd361ca8e99f45
Bobvary <bobvaryemmanuel2@gmail.com>
2024-01-10 19:53:14 +0800

    added first files

---
+++ new.py
@@ -0,0 +1,8 @@
+import math
+
+firstNumber = input("we welcome you to our website. Please input your first number" + "\n")
+firstNumber = int(firstNumber)
+secondNumber = input("Thank you. Please input your second number" + "\n")
+secondNumber = int(secondNumber)
+total = firstNumber + secondNumber
+print("hurray, your total is " ,total)
